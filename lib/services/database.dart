4d089303783c1f2d9645cc8cc8fda7eac33876f0
iamsirmikel <asamoahmichael77@gmail.com>
2020-07-02 15:20:26 +0000

    added database queries

---
+++ lib/services/database.dart
@@ -0,0 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final _firestore = Firestore.instance;
+
+class Queries {
+  Future<void> saveprofile(
+      _name, _email, _phone, _experience, _githublink, _resume) async {
+    await _firestore.collection('profile').add({
+      'name': _name,
+      'email': _email,
+      'phone': _phone,
+      'experience': _experience,
+      'githublink': _githublink,
+      'resume': _resume,
+    });
+  }
+}
