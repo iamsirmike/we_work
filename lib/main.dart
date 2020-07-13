197db7cb7598f25dee8d22b836d8979069ad8eaa
iamsirmikel <asamoahmichael77@gmail.com>
2020-07-13 21:40:11 +0000

    changed the app title

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
+        title: 'weW',
         theme: ThemeData(
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
